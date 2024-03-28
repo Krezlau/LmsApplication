a82fbb9ba9a000ae3a7cb1b4d3e8134011817859
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2024-03-28 15:57:38 +0100

    initial

---
+++ LmsApplication/LmsApplication.AppHost/Program.cs
@@ -0,0 +1,11 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var authService = builder.AddProject<Projects.LmsApplication_Api_AuthService>("authService");
+
+var apiGateway = builder.AddProject<Projects.LmsApplication_Api_Gateway>("apiGateway")
+    .WithReference(authService);
+
+builder.AddProject<Projects.LmsApplication_Web>("webfrontend")
+    .WithReference(apiGateway);
+
+builder.Build().Run();
