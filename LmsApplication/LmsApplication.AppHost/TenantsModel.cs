047a7df62333868648f67e886e769c8dcf60debd
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2024-05-05 19:50:31 +0200

    fix apphost

--- LmsApplication/LmsApplication.AppHost/TenantsModel.cs
+++
@@ -1,8 +0,0 @@
-namespace LmsApplication.Core.Config.ConfigModels;
-
-public class TenantsModel
-{
-    public static string Key = "App:Tenants";
-    
-    public required string[] Tenants { get; set; }
-}
