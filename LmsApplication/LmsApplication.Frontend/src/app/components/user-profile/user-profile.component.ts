796aabe6f7d19107465e201bd77afb7644c63f38
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2024-11-01 22:33:50 +0100

    [FE] Add profile page

--- LmsApplication/LmsApplication.Frontend/src/app/components/user-profile/user-profile.component.ts
+++ LmsApplication/LmsApplication.Frontend/src/app/components/user-profile/user-profile.component.ts
@@ -1,12 +1,27 @@
 import { Component, Input } from '@angular/core';
 import { UserModel } from '../../types/users/user-model';
+import { NgIf } from '@angular/common';
+import { UserRole, toHumanReadable } from '../../types/users/user-role';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './user-profile.component.html',
 })
 export class UserProfileComponent {
   @Input() user: UserModel | null = null;
+
+  constructor(private authService: AuthService) {}
+
+  authState = this.authService.authState;
+
+  toHumanReadable(role: UserRole) {
+    return toHumanReadable(role);
+  }
+
+  isLoggedInUser() {
+    return this.user?.id == (this.authState().userData?.id ?? '');
+  }
 }
